exercism/rust

commit 0e50631fd7172eb2aceafd0e7792b22c295ab158

anagram, forth: Use `&str` instead of `&String`
Clippy suggested:
https://travis-ci.org/exercism/rust/builds/456356560
warning: writing `&String` instead of `&str` involves a new object where a slice will do.
   = help: for further information visit https://rust-lang-nursery.github.io/rust-clippy/v0.0.212/index.html#ptr_arg

exercises/forth/example.rs
@@ -92,7 +92,7 @@ impl Forth {
             .or_else(|_| self.step_built_in(&word))
     }
 
-    fn step_built_in(&mut self, word: &String) -> ForthResult {
+    fn step_built_in(&mut self, word: &str) -> ForthResult {
         match word.as_ref() {
             "+" => self.bin_op(|(a, b)| Ok(a + b)),
             "-" => self.bin_op(|(a, b)| Ok(a - b)),

exercises/anagram/example.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-fn sort(word: &String) -> String {
+fn sort(word: &str) -> String {
     let mut sorted: Vec<char> = word.chars().collect();
     sorted.sort();
     sorted.into_iter().collect()

GoldenPath1109/exercism-rust

commit 6ed9b87e1a9a36815aedf19fff796265a180c5fa

forth: Remove unneeded `as_ref`
Clippy suggested
https://travis-ci.org/exercism/rust/builds/456432005
warning: this call to `as_ref` does nothing
   = help: for further information visit https://rust-lang-nursery.github.io/rust-clippy/v0.0.212/index.html#useless_asref

(Caused by 0e50631fd7172eb2aceafd0e7792b22c295ab158)

exercises/forth/example.rs
@@ -93,7 +93,7 @@ impl Forth {
     }
 
     fn step_built_in(&mut self, word: &str) -> ForthResult {
-        match word.as_ref() {
+        match word {
             "+" => self.bin_op(|(a, b)| Ok(a + b)),
             "-" => self.bin_op(|(a, b)| Ok(a - b)),
             "*" => self.bin_op(|(a, b)| Ok(a * b)),